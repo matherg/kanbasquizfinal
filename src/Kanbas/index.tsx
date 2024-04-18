8ec52d97e1cbd8d5db15b285386fcecbf4390243
Graham Mather <mather.g@northeastern.edu>
2024-04-18 14:39:25 -0400

    a6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import store from "./store";
 import { Provider } from "react-redux";
+import Account from "./Account";
 
 const API_BASE = process.env.REACT_APP_API_BASE;
 
@@ -61,7 +62,7 @@ function Kanbas() {
             <div>
                 <Routes>
                     <Route path="/" element={<Navigate to="Dashboard" />} />
-                    <Route path="Account" element={<h1>Account</h1>} />
+                    <Route path="/Account/*" element={<Account />} />
                     <Route path="Dashboard" element={
                         <Dashboard
                             courses={courses}
